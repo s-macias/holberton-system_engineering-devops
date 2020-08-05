df100d1f2baa26807c60e63dd420128921b7d6f0
s-macias <1552@holbertonschool.com>
2020-08-05 04:04:25 +0000

    Task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,8 +1,7 @@
-
 # puppet file to modify configuration file
 # It refuses to ask for a password and adds a private key
 file_line { 'etc/ssh/ssh_config':
-    path         => '~/.ssh/config',
+    path         => '/etc/ssh/ssh_config',
     line         => 'IdentityFile ~/.ssh/holberton',
     line         => 'PasswordAuthentication no',
 }
