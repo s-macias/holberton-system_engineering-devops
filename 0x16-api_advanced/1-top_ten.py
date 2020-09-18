5977413a45620864ba40d329439852ac996793a3
s-macias <1552@holbertonschool.com>
2020-09-18 01:56:05 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit. """
+
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """ Prints the titles of the first 10 hot posts for a subreddit """
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    header = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    try:
+        top = requests.get(
+            url, headers=header, allow_redirects=False).json().get('data').get('children')
+        for item in range(10):
+            print(top[item].get('data').get('title'))
+    except:
+        print ("None")
